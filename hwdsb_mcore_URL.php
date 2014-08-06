f1080986d4b601bb5f8cd4cb40252d8f12ad9f7e
mrjarbenne <jarbenne@hwdsb.on.ca>
2014-08-05 21:56:38 -0400

    Initial Commit

---
+++ hwdsb_mcore_URL.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name:    HWDSB Mediacore URL 
+Description:    Registers the Mediacore URL for all sites
+Version:        1.0
+Author:         Bob Fowler & Jared Bennett
+*/
+
+class HWDSB_mcore_URL {
+	
+	
+	/**
+	 * Static init method.
+	 */
+	public static function init() {
+		return new self();
+	}
+	
+	/**
+	 * Constructor.
+	 */
+    public function __construct() {
+        add_filter( 'pre_option_mcore_url', array( 'HWDSB_mcore_URL', 'get_mc_key' ) );
+    }
+	
+	/**
+	 * Returns VP license key
+	 */
+	public function get_mc_key($mcore_url) {
+		$mcore_url = 'https://tv.hwdsb.on.ca';
+		return $mcore_url;
+	}
+}
+
+HWDSB_mcore_URL::init();
